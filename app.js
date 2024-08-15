bc4453671214e03c5b950cf102cfea8cbc75792f
HenL188 <153272326+HenL188@users.noreply.github.com>
2024-08-15 14:09:33 -0600

    secound commit

--- app.js
+++ app.js
@@ -1,3 +1,50 @@
-document.getElementById("login").onclick = function () {
-    window.location.href = "/account.html";
+var username = "username";
+var password = "password";
+
+function login() {
+    var errorElement = document.getElementById("username")
+    var input = document.getElementById("username").value;
+    var input2 = document.getElementById("password").value;
+    if (input != username || input2 != password) {
+        errorElement.textContent = ("Incorrect password or username");
+        errorElement.classList.remove("")
+    }
+    else {
+        window.location.href = "/account.html";
+    }
 };
+
+function newLogin() {
+    var newpassword = document.getElementById("newpassword").value;
+    var repassword = document.getElementById("renterpassword").value;
+    var username = document.getElementById("newusername").value;
+    var e = document.getElementById("error");
+
+    if (username === "") {
+        e.textContent = "Invaild username";
+        e.style.color = "red";
+    }
+    else if (newpassword === "" || repassword === "") {
+        e.textContent = "Invaild password";
+        e.style.color = "red";
+    }
+    else {
+        e.textContent = "";
+        if (newpassword != repassword) {
+            //e.innerHTML = "<span style='color: red;'>" + "Passwords do not match</span>";
+            e.textContent = "Passwords do not match";
+            e.style.color = "red";
+
+        }
+        else {
+            //e.innerHTML = "";
+            e.textContent = "";
+            username = document.getElementById("newusername").value;
+            password = document.getElementById("newpassword").value;
+            window.location.href = "/account.html";
+        }
+    }
+
+
+};
+
